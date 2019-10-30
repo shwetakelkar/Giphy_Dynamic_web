ba55e4a3752136c8e0ccbd198fd624fa6f5bcb98
shwetakelkar <shweta4.gupta@gmail.com>
2019-10-29 22:31:56 -0500

    final commit with all the changes including styling and checkpoints

--- assets/javascript/giphy-web.js
+++ assets/javascript/giphy-web.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
+
     var btnArray = ["tabla","recorder","flute","viola","harp","bongo drum","sitar","guitar","woodwinds","bass guitar","banjo","piano","drum","violin","rubab","garmon","keyboard","clarinet","xylophone","trumpet","tuba","saxophone","cello"];
+
+    const limit = 10;//variable to define limit
+
     /*-------------------------------------------------------------
      the btnArray will be filled once at the time of page load.
     --------------------------------------------------------------*/
@@ -11,41 +15,66 @@ $(document).ready(function(){
         }
     }
 
-    
     /*-------------------------------------------------------------
     this part of code execute .on click of submit button.
     and create new button/s with the label of input text.
-    and puch the value of new instrument in btnArray so it won't
-    get duplicated in future. 
+    and push the value of new instrument in btnArray so it won't
+    get duplicated in future.
+    as well as some checkes for
+    1. at the time of adding new button if any button is in active
+       state then replace with "" 
+    2. empty input value check and respective msg
+    3. duplication checkpoint with respective msg 
+    at last clearing the input text field
     --------------------------------------------------------------*/
-    $(".btn").on("click",function(e){
+    $("#submit-btn").on("click",function(e){
         
         e.preventDefault();
-        var inputTextVal = $("#instru-name").val();
-        $("#instru-name").val("");
-        
+        $(".alert").remove();
+        var inputTextVal = $("#input-name").val().trim();
         
         if(btnArray.indexOf(inputTextVal) == -1 && inputTextVal)
         {
-            ;
             var newbtn = $("<button>").attr("class","btn-primary btn focus-cls m-1").text(inputTextVal).appendTo("#btn-row");
             btnArray.push(inputTextVal);
+            var current = document.getElementsByClassName("active");
+        
+            if(current.length>0)
+            {
+                current[0].className = current[0].className.replace("active", "");
+            }
             
         }
         else if(btnArray.indexOf(inputTextVal) !== -1){
-            $("<p>").text("already exist").appendTo(".col-md-3");
+            $("<p>").addClass("alert").text("Given input is already exist!!").appendTo(".col-md-3");
         }
+        else if(!inputTextVal)
+        {
+            $("<p>").addClass("alert").text("please enter the value!!").appendTo(".col-md-3");
+        }
+        $("#input-name").val("");
     });
 
     /*---------------------------------------------------
     This .on("click") function will trigger the AJAX Call
+    before it making the button display as selected
+    and currently active  
     ------------------------------------------------------*/
     
     $("#btn-row").on("click",'button',function(e){
 
-        
-        $("#img-col").empty();
         e.preventDefault();
+        $("#img-col").empty();
+        $(".alert").remove();
+
+        var current = document.getElementsByClassName("active");
+        
+        if(current.length>0)
+        {
+            current[0].className = current[0].className.replace("active", "");
+        }
+        this.className += " active";
+        
         
         var instrument = $(this).text();
         
@@ -61,9 +90,11 @@ $(document).ready(function(){
         });
 
     });
+
     /*----------------------------------------------------------------
-        This .on("click") function will trigger state change of images 
-        eg; still -> animate , animate ->still
+        This .on("click") function will trigger state change of selected 
+        image 
+eg; still -> animate , animate ->still
     ----------------------------------------------------------------*/
     $("#img-col").on("click","img", function(){
                 
@@ -83,7 +114,8 @@ $(document).ready(function(){
 
         }
     })
-});
+
+
 /*----------------------------------------------------------------
 This function call will be placed after getting succesful response
 It will create new elements to show the images of selection
@@ -91,20 +123,21 @@ It will create new elements to show the images of selection
 arguments: response
 return : none
 ----------------------------------------------------------------*/
-const limit = 10;
-function displayImages(response) {
-    var imgDiv = $("<div class='row'>").appendTo("#img-col");
-    for (var i = 0; i < limit; i++) {
-        if (response.data[i].rating === "g" || response.data[i].rating === "pg") {
-            var imgToDisplay = $("<img width=200px>");
-            var imgCol = $("<div class='col-lg-3 col-md-4 mr-1 mt-2'>").appendTo(imgDiv);
-            $("<p class= 'mb-3'>").text("Rating : " + response.data[i].rating).appendTo(imgCol);
-            imgToDisplay.attr({
-            "data-state": "still", "class": "gif", "src": response.data[i].images.fixed_height_small_still.url,
-                "data-animate": response.data[i].images.fixed_height_small.url,
-                "data-still": response.data[i].images.fixed_height_small_still.url
-            });
-            imgToDisplay.appendTo(imgCol);
+
+    function displayImages(response) {
+        var imgDiv = $("<div class='row'>").appendTo("#img-col");
+        for (var i = 0; i < limit; i++) {
+            if (response.data[i].rating === "g" || response.data[i].rating === "pg") {
+                var imgToDisplay = $("<img>");
+                var imgCol = $("<div class='col-lg-3 col-md-4 mr-1 mt-2'>").appendTo(imgDiv);
+                $("<p class= 'mb-3'>").text("Rating : " + response.data[i].rating).appendTo(imgCol);
+                imgToDisplay.attr({
+                "data-state": "still", "class": "gif", "src": response.data[i].images.fixed_height_small_still.url,
+                    "data-animate": response.data[i].images.fixed_height_small.url,
+                    "data-still": response.data[i].images.fixed_height_small_still.url
+                });
+                imgToDisplay.appendTo(imgCol);
+            }
         }
     }
-}
+});
